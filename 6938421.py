8524b42307d8fcdf4f16a4dc8426e6714fe9274b
bnhussein <126253857+bnhussein@users.noreply.github.com>
2023-02-24 14:17:16 +0000

    Add files via upload

---
+++ 6938421.py
@@ -0,0 +1,74 @@
+#this script give details about cars and their prices
+import sys
+hello=input('hello welcome to ENCRIS CAR DEALINGS, if you want to proceed input yes: ')
+if hello == 'yes' :
+    print('welcome once again')
+else :
+    sys.exit(0) #at this point
+results=input('which type of car do you want?: ')
+if results == str('benz gle63s coupe') :
+    print('the price is 200,000 dollars. thank you.')
+if results == str('lamborghini urus') :
+    print('the price is 400,000 dollars. thank you.')
+if results == str('chevrolet camaro 2SS') :
+    print('the price is 60,000 dollars. thank you.')
+if results == str('range rover sport') :
+    print('the price is 100,000 dollars. thank you.')
+if results == str('range rover evoque') :
+    print('the price is 70,000 dollars. thank you.')
+if results == str('mercedes benz s class') :
+    print('the price is 520,000 dollars thank you.')
+if results == 'range rover vogue' :
+    print('the price is 210,000 dollars. thank you.')
+if results == 'toyota corolla s' :
+    print('the price is 20,000 dollars. thank you.')
+if results == 'toyota camry' :
+    print('the price is 30,000 dollars. thank you.')
+if results == 'bentley bentaga' :
+    print('the price is 410,000 dollars. thank you.')
+if results == 'lamborghini adventador' :
+    print('the price is 250,000 dollars. thank you.')
+if results == 'toyota elantra' :
+    print('the price is 20,000 dollars. thank you.')
+if results == 'lamborghini hurrican' :
+    print('the price is 250,000 dollars. thank you.')
+if results == 'toyota camry spider' :
+    print('the price is 290,000 dollars. thank you.')
+if results == 'range rover velar' :
+    print('the price is 90,000 dollars. thank you.')
+if results == 'kia' :
+    print('the price is 10,000 dollars. thank you.')
+if results == 'bugatti cheron' :
+    print('the price is 5,000,000 dollars. thank you.')
+if results == 'chevrolet camaro 1ss' :
+    print('the price is 52,000 dollars. thank you.')
+if results == 'bmw x5' :
+    print('the price is 120,000 dollars. thank you.')
+if results == 'honda accord' :
+    print('the price is 25,000 dollars. thank you.')
+if results == 'hyundai' :
+    print('the price is 13,000 dollars. thank you.')
+if results == 'toyota corolla' :
+    print('the price is 7,000 dollars. thank you.')
+if results == 'tesla' :
+    print('the price is 350,000 dollars. thank you.')
+if results == 'bentley coupe' :
+    print('the price is 280,000 dollars. thank you.')
+if results == 'rolls royce cullinan' :
+    print('the price is 490,000 dollars. thank you.')
+if results == 'roys royce wraith' :
+    print('the price is 220,000 dollars. thank you.')
+if results == 'bmw x4' :
+    print('the price is 100,000 dollars. thank you.')
+if results == 'mclaren 720s' :
+    print('the price is 300,000 dollars. thank you.')
+if results == 'ferrari 488' :
+    print('the price is 720,000 dollars. thank you.')
+if results == 'dodge hellcat' :
+    print('the price is 65,000 dollars. thank you.')
+else :
+    sys.exit(0)
+    
+    
+ #github account url= https://github.com/bnhussein/Data-Structures.git
+ name:bmhussein
